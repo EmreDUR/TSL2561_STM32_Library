69f114a505c69f3b5916642a7ac49c8c05d8c214
EmreDUR <emre254254@gmail.com>
2023-02-11 15:18:15 +0300

    First commit

---
+++ TSL2561.c
@@ -0,0 +1,9 @@
+/*
+ * TSL2561.c
+ *
+ *  Created on: Feb 11, 2023
+ *      Author: acer
+ */
+
+#include "TSL2561.h"
+
---
+++ TSL2561.h
@@ -0,0 +1,17 @@
+/*
+ * TSL2561.h
+ *
+ *  Created on: 11 February 2023
+ *      Author: Emre Dur
+ */
+
+#ifndef TSL2561_I2C_DRIVER_H_
+#define TSL2561_I2C_DRIVER_H_
+
+//Include HAL Library by including the main header from the created project
+#include "main.h"
+
+
+
+
+#endif /* TSL2561_I2C_DRIVER_H_ */
